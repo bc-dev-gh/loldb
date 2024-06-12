c0e48b25c4348816c0a8ad538a93b752486a934c
Bcheow <59217063+Bcheow@users.noreply.github.com>
2024-06-11 21:28:50 -0700

    rebuilding routing for ghpages

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -5,9 +5,9 @@ export default function MainNav() {
     <nav className='main-nav'>
     <img className='nav-logo' alt='nav logo' src='https://brand.riotgames.com/static/a91000434ed683358004b85c95d43ce0/8a20a/lol-logo.png'/>
       <ul className='nav-list'>
-        <li><a href='/#'>Home</a></li>
-        <li><a href='/#champions'>Champions</a></li>
-        <li><a href='/#items'>Items</a></li>
+        <li><a href='/loldb/#'>Home</a></li>
+        <li><a href='/loldb/#champions'>Champions</a></li>
+        <li><a href='/loldb/#items'>Items</a></li>
         <li className='push-right'>About</li>
         <li>Stuff</li>
       </ul>
